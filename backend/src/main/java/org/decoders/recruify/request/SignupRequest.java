c1492e3cf315b7ec5138aaddd472641c8ba36aac
Sai Kumar <ksai44576@gmail.com>
2025-01-18 17:34:41 +0530

    Add JSON property annotations to request and response classes for improved serialization

--- backend/src/main/java/org/decoders/recruify/request/SignupRequest.java
+++ backend/src/main/java/org/decoders/recruify/request/SignupRequest.java
@@ -1,12 +1,16 @@
 package org.decoders.recruify.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class SignupRequest {
     private String email;
     private String password;
+    @JsonProperty("full_name")
     private String fullName;
+    @JsonProperty("user_type")
     private String userType;
+    @JsonProperty("user_name")
     private String userName;
 }
